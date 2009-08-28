c7a005fade0b13cf10404a8c76c72555d936d413
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-08-28 00:39:58 -0600

    include will_paginate agnostic, much hacking, posts showing up

--- darkblog.rb
+++ darkblog.rb
@@ -1,22 +1,29 @@
 #!/Users/darkhelmet/local/ruby/bin/ruby
 
+$KCODE = 'u'
+
+$: << File.expand_path(File.join('.', 'lib'))
+
 require 'rubygems'
 require 'sinatra'
+require 'haml'
 require 'activerecord'
-require 'will_paginate' 
-require 'will_paginate/view_helpers'
-require 'acts_as_taggable'
-require 'tags_helper'
+require 'activesupport'
+require 'will_paginate'
+require 'will_paginate/finders/active_record'
+require 'will_paginate/view_helpers/base'
+require 'will_paginate/view_helpers/link_renderer'
+%w(acts_as_taggable tag tag_list tagging tags_helper).each { |lib| require lib }
 require 'ostruct'
+require 'RedCloth'
+require 'sinatra/authorization'
+require 'models'
 
-WillPaginate.enable_activerecord
-
-WillPaginate::LinkRenderer.class_eval do
+WillPaginate::ViewHelpers::LinkRenderer.class_eval do
 protected
   
   def url(page)
-    debugger
-    url = @template.request.url
+    url = request.url
     if 1 == page
       '/'
     else
@@ -24,11 +31,6 @@ protected
     end
   end
 end
-
-$: << File.expand_path(File.join('.', 'lib'))
-
-require 'sinatra/authorization'
-require 'models'
 
 configure do
   Blog = OpenStruct.new(:title => ENV['BLOG_TITLE'] || 'verbose logging',
@@ -41,13 +43,14 @@ configure do
                         :password => ENV['BLOG_PASSWORD'] || 'password',
                         :github => ENV['BLOG_GITHUB'] || 'darkhelmet',
                         :twitter => ENV['BLOG_TWITTER'] || 'darkhelmetlive',
-                        :delicious => ENV['BLOG_DELICIOUS'] || 'darkhelmetlive')
+                        :delicious => ENV['BLOG_DELICIOUS'] || 'darkhelmetlive',
+                        :per_page => ENV['BLOG_PER_PAGE'] || 10)
 end
 
 helpers do
   include Sinatra::Authorization
   include TagsHelper
-  include WillPaginate::ViewHelpers
+  include WillPaginate::ViewHelpers::Base
   
   def partial(page, options = {})
     haml(page, options.merge(:layout => false))
@@ -59,12 +62,17 @@ helpers do
   end
   
   def javascript_include_tag(js)
-    link = js.include?('http://') ? link : "/javascripts/#{js}.js"
+    link = js.include?('http://') ? js : "/javascripts/#{js}.js"
     partial("%script{ :type => 'text/javascript', :src => '#{link}' }")
   end
   
+  def image_tag(img, alt = img.split('/').last)
+    link = img.include?('http://') ? img : "/images/#{img}"
+    partial("%img{ :src => '#{link}', :alt => '#{alt}' }")
+  end
+  
   def title
-    @title
+    @title || Blog.title
   end
   
   def title=(t)
@@ -74,15 +82,46 @@ helpers do
   def fb_url
     "http://feeds.feedburner.com/#{Blog.feedburner}"
   end
+  
+  def gravatar_url(email)
+    "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(email)}"
+  end
+  
+  def tag_link(tag)
+    partial("%a{ :href => '/tags/#{tag}', :rel => 'tag' } tag")
+  end
+  
+  def tag_links(post)
+    post.tag_list.map { |tag| tag_link(tag) }.join(' ')
+  end
+  
+  def post_class(post)
+    post.tag_list.map { |tag| "tag-#{tag}" } | ['post', "post-#{post.id}", "category-#{post.category}"]
+  end
+  
+  def post_permalink(post)
+    "/#{post.published_on.strftime('%y/%m/%d')}/#{post.slug}"
+  end
 end
 
 # main index
 get '/' do
+  @posts = Post.paginate(:page => 1, :per_page => Blog.per_page)
   haml(:posts)
 end
 
 # rss feed
 get '/feed' do
+end
+
+get '/google-search' do
+  haml(:page, :locals => { :page => :google })
+end
+
+%w(about contact disclaimer).each do |page|
+  get "/#{page}" do
+    haml(:page, :locals => { :page => page.intern })
+  end
 end
 
 # pagination
--- lib/models.rb
+++ lib/models.rb
@@ -1,7 +1,27 @@
 class Post < ActiveRecord::Base
   belongs_to :redirection, :class_name => 'Post'
+  validates_presence_of :title
+  validates_presence_of :category
+  
   acts_as_taggable
+  
+  default_scope(:order => 'published_on DESC', :published => false)
+  named_scope(:published, :conditions => { :published => true })
+  
+  before_save do |record|
+    record.slug = record.title.parameterize
+    record.published_on  = Time.now unless record.published_on?
+  end
+  
+  def body_html
+    RedCloth.new(body.gsub("\r\n\r\n","\r\n<br />\r\n")).to_html
+  end
+  
+  def matches_url(year,month,day)
+    created_at.year == year && created_at.month == month && created_at.day
+  end
 end
+
 
 env = ENV.has_key?('RACK_ENV') ? ENV['RACK_ENV'].to_sym : :development
 CONFIG_FILE = File.expand_path(File.join(File.dirname(__FILE__), '..', 'config', 'database.yml'))
