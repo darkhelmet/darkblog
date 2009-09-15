a3fceeb4e4d891e8ea5343507645994fd61327fc
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-09-14 21:00:16 -0600

    etag fix...

--- lib/rack/etag.rb
+++ lib/rack/etag.rb
@@ -10,15 +10,7 @@ module Rack
     def call(env)
       status, headers, body = @app.call(env)
       if !headers.has_key?('ETag')
-        val = case body
-              when String
-                body
-              when Array
-                f = body.first
-                f.is_a?(String) ? f : nil
-              else
-                nil
-              end
+        val = body.nil? ? nil : body.to_s
         headers['ETag'] = %("#{Digest::MD5.hexdigest(val)}") unless val.nil?
       end
       [status, headers, body]
