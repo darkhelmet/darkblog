630a6344935086e30b3b647c473807eeaf61f982
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-09-17 22:32:47 -0600

    fix up inline

--- public/javascripts/jquery.darkhax.js
+++ public/javascripts/jquery.darkhax.js
@@ -7,16 +7,18 @@ var lightboxVars = { imageLoading: 'http://s3.blog.darkhax.com/lightbox-ico-load
 $(document).ready(function() {
   $('a.remote-inline').live('click', function() {
     var link = $(this);
+    var b = link.closest('.content').prev();
+    var r = link.parent();
     link.replaceWith('Loading...');
     $.ajax({url: this.href,
             type: 'GET',
             success: function(data) {
-              $(link.closest('.content').prev()).before($(data).addClass('new-elem').css('display','none'));
+              r.remove();
+              b.before($(data).addClass('new-elem').css('display','none'));
               $('.new-elem').slideDown('slow', function() {
                 $(this).removeClass('new-elem');
               });
             }});
-    link.parent().remove();
     return false;
   });
   $('a.lightbox').lightBox(lightboxVars);
