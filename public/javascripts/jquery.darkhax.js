8a0701e36605df56fce7912b090cffb2f3bb500b
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-09-17 22:05:24 -0600

    inline changes

--- public/javascripts/jquery.darkhax.js
+++ public/javascripts/jquery.darkhax.js
@@ -1,26 +1,3 @@
-$.fn.setupRemoteInline = function() {
-    return this.each(function() {
-            $(this).click(function() {
-                    var link = $(this);
-                    link.unbind('click');
-                    link.html('Loading...');
-                    $.ajax({
-                        url:this.href,
-                        type:'GET',
-                        success:function(data) {
-                                d = $(data).addClass('new-elem').css('display','none');
-                                $(link.closest('.content').prev()).before(d);
-                                $('.new-elem').slideDown('slow', function() {
-                                        $(this).removeClass('new-elem');
-                                    });
-                                link.parent().remove();
-                                $('a.remote-inline').setupRemoteInline();
-                            }});
-                    return false;
-                });
-        });
-};
-
 var lightboxVars = { imageLoading: 'http://s3.blog.darkhax.com/lightbox-ico-loading.gif',
                      imageBtnClose: 'http://s3.blog.darkhax.com/lightbox-btn-close.gif',
                      imageBtnPrev: 'http://s3.blog.darkhax.com/lightbox-btn-prev.gif',
@@ -28,6 +5,19 @@ var lightboxVars = { imageLoading: 'http://s3.blog.darkhax.com/lightbox-ico-load
                      imageBlank: 'http://s3.blog.darkhax.com/lightbox-blank.gif' }
 
 $(document).ready(function() {
-        $('a.remote-inline').setupRemoteInline();
-        $('a.lightbox').lightBox(lightboxVars);
-    });
+  $('a.remote-inline').live('click', function() {
+    var link = $(this);
+    link.replaceWith('Loading...');
+    $.ajax({url: this.href,
+            type: 'GET',
+            success: function(data) {
+              link.closest('.content').prev().before($(data).addClass('new-elem').css('display','none'));
+              $('.new-elem').slideDown('slow', function() {
+                $(this).removeClass('new-elem');
+              });
+            }});
+    link.parent().remove();
+    return false;
+  });
+  $('a.lightbox').lightBox(lightboxVars);
+});
