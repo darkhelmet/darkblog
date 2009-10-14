070c98635c8a69126d6cffa034d08c4ee45deca2
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-10-14 00:39:35 -0600

    try this for heroku

--- darkblog.rb
+++ darkblog.rb
@@ -365,6 +365,7 @@ named_routes[:posts] = '/posts'
 named_routes[:redirections] = '/redirections'
 named_routes[:update_twitter] = '/update-twitter'
 named_routes[:admin_index] = '/index'
+named_routes[:sitemap_xsl] = '/sitemap.xsl'
 
 # main index with pagination
 # get %r|^/(?:page/(\d+))?$| do |page|
@@ -539,4 +540,10 @@ named_route(:get, :admin_index) do
   require_administrative_privileges
   @posts = Post.unpublished.all
   haml(:admin_index, :layout => :admin)
+end
+
+# maybe this will work
+named_route(:get, :sitemap_xsl) do
+  content_type('application/xml', :charset => 'utf-8')
+  File.read(File.join('.', 'public', 'sitemap.xsl'))
 end
