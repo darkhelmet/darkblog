72cce6af7aad79b4db1bee7fa0f4df6212c30ddc
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2009-11-02 20:10:48 -0700

    return false on clicks so the url doesn't change

--- public/javascripts/panel.js
+++ public/javascripts/panel.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
-	
 	// Expand Panel
 	$("#open").click(function(){
 		$("div#panel").slideDown("slow");
-	
-	});	
+		return false;
+	});
 	
 	// Collapse Panel
 	$("#close").click(function(){
-		$("div#panel").slideUp("slow");	
-	});		
+		$("div#panel").slideUp("slow");
+		return false;
+	});
 	
 	// Switch buttons from "Log In | Register" to "Close Panel" on click
 	$("#toggle a").click(function () {
 		$("#toggle a").toggle();
-	});		
-		
+		return false;
+	});
 });
