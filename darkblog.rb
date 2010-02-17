294d25f4b7a7c555c85716aef61a9880b27d8040
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-02-17 01:18:11 -0700

    back to .gems since bundler 0.9 failed me

--- darkblog.rb
+++ darkblog.rb
@@ -4,18 +4,21 @@ $KCODE = 'u' if RUBY_VERSION.match(/1\.8/)
 
 $: << File.expand_path(File.join('.', 'lib'))
 
-begin
-  # Require the preresolved locked set of gems.
-  require File.expand_path('../.bundle/environment', __FILE__)
-rescue LoadError
-  # Fallback on doing the resolve at runtime.
-  require 'rubygems'
-  require 'bundler'
-  Bundler.setup
-end
+# begin
+#   # Require the preresolved locked set of gems.
+#   require File.expand_path('../.bundle/environment', __FILE__)
+# rescue LoadError
+#   # Fallback on doing the resolve at runtime.
+#   require 'rubygems'
+#   require 'bundler'
+#   Bundler.setup
+# end
+#
+# require 'sinatra'
+# Bundler.require
 
+require 'rubygems'
 require 'sinatra'
-Bundler.require
 
 require 'blog_helper'
 
