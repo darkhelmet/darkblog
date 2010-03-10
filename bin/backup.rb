69a29b23d6a60ca03e97e1b9c74d0a80e030b329
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-03-09 21:31:26 -0700

    backup the json dump

--- bin/backup.rb
+++ bin/backup.rb
@@ -1,9 +1,12 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'yaml'
 require 'aws/s3'
+require 'restclient'
 
 S3_CREDENTIALS = File.expand_path(File.join('~', '.s3', 'auth.yml'))
+HTTP_CREDENTIALS = File.expand_path(File.join('~', '.darkblog', 'auth.yml'))
 APP = 'darkblog'
 BUCKET = 's3.blog.darkhax.com'
 
@@ -11,43 +14,44 @@ def bundle
   `heroku bundles --app #{APP}`.split(/\s/).first
 end
 
-def destroy
-  system("heroku bundles:destroy #{bundle} --app #{APP}")
-end
-
-def create
-  system("heroku bundles:capture --app #{APP}")
-end
-
 def capturing?
   !`heroku bundles --app #{APP}`.match('capturing').nil?
 end
 
-def download
-  `heroku bundles:download --app #{APP}`.split(' ').last
-end
-
-def key
-  Time.now.strftime('%m-%d-%Y-%H-%M.tar.gz')
-end
-
-def backup!
-  destroy
-  create
-
+def capture
+  system("heroku bundles:destroy #{bundle} --app #{APP}")
+  system("heroku bundles:capture --app #{APP}")
   print 'Waiting for capture to finish...'
   while capturing?
     print '.'
     sleep(1)
   end
   print "done!\n"
-
-  AWS::S3::Base.establish_connection!(YAML.load_file(S3_CREDENTIALS))
-  archive = download
-  File.open(archive, 'rb') do |f|
-    AWS::S3::S3Object.store("/backups/#{key}", f, BUCKET)
-  end
-  print "Done!\n"
 end
 
-backup!
+def download
+  `heroku bundles:download --app #{APP}`.split(' ').last
+end
+
+def key(ext)
+  Time.now.strftime("%m-%d-%Y-%H-%M.#{ext}")
+end
+
+def backup_bundle!
+  capture
+  archive = download
+  File.open(archive, 'rb') do |f|
+    AWS::S3::S3Object.store("/backups/#{key('tar.gz')}", f, BUCKET)
+  end
+end
+
+def backup_json!
+  user, pass = YAML.load_file(HTTP_CREDENTIALS)
+  json = RestClient.get("http://#{user}:#{pass}@blog.darkhax.com/dump.json")
+  AWS::S3::S3Object.store("/backups/#{key('json')}", json, BUCKET)
+end
+
+AWS::S3::Base.establish_connection!(YAML.load_file(S3_CREDENTIALS))
+backup_bundle!
+backup_json!
+print "Done!\n"
