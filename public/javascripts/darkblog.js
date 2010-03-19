ca286a65e0ba663473aaa1736f087347d82a028f
Daniel Huckstep <darkhelmet@darkhelmetlive.com>
2010-03-18 21:51:36 -0600

    Fix BugID: 443

--- public/javascripts/darkblog.js
+++ public/javascripts/darkblog.js
@@ -53,6 +53,18 @@ function ReaderBadge(json) {
 }
 
 (function($) {
+  $.expr[':'].regex = function(elem, index, match) {
+    var matchParams = match[3].split(',');
+    var validLabels = /^(data|css):/;
+    var attr = {
+      method: matchParams[0].match(validLabels) ? matchParams[0].split(':')[0] : 'attr',
+      property: matchParams.shift().replace(validLabels,'')
+    };
+    var regexFlags = 'ig';
+    var regex = new RegExp(matchParams.join('').replace(/^\s+|\s+$/g,''), regexFlags);
+    return regex.test(jQuery(elem)[attr.method](attr.property));
+  };
+
   $.extend(jQuery, {
     githubBadge: function(username) {
       $.getScript('http://github.com/api/v1/json/' + username + '?callback=GithubBadge');
@@ -133,7 +145,8 @@ $(document).ready(function() {
         backgroundImagize(this, $(img));
         img.remove();
       });
-      $('.entry a.img').facebox();
+      $('.entry a:regex(href, png|jpe?g|gif).img').facebox();
+      backgroundizeImages();
     } else {
       setTimeout(backgroundizeLinkImages, 100);
     }
@@ -153,8 +166,6 @@ $(document).ready(function() {
       setTimeout(backgroundizeImages, 100);
     }
   };
-
-  backgroundizeImages();
 
   $('a.remote-inline').live('click', function() {
     var link = $(this);
